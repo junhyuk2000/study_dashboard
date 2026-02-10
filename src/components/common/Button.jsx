2960335eeaff375394cae760f8637a16e42bb1fc
junhyuk2000 <wnscjcj2000@naver.com>
2026-02-10 19:04:16 +0900

    style: 로그인 화면 UI 스타일링

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,20 @@
+import '../../styles/Button.css';
+
+export default function Button ({
+    children,
+    variant = "",
+    disabled = false,
+    className="",
+    ...props
+}) {
+    return (
+        <button 
+            className={`btn btn-${variant} {className}`}
+            disabled={disabled}
+            {...props}
+        >
+            {children}
+        </button>
+    )
+    
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
-
+import "../styles/login.css"
+import { MdCalendarMonth } from "react-icons/md";
+import Button from "../components/common/Button";
 
 export default function Login() {
     const navigate = useNavigate();
@@ -8,9 +10,43 @@ export default function Login() {
         navigate("/dashboard");
     }
     return (
-        <div>
-            <h1>Login Page</h1>
-            <button onClick={handleLogin}>login</button>
+        <div className="login-container">
+            <div className="login-box">
+                <div className="design-box">
+                    <span className="dot red"></span>
+                    <span className="dot yellow"></span>
+                    <span className="dot green"></span>
+                    <span className="address-bar"></span>
+                </div>
+
+
+                <div className="login-title">
+                    <MdCalendarMonth className="calendar-icon"/>
+                    <h1 className="title-name">My DashBoard</h1>
+                </div>
+                <p>로그인</p>
+                
+                <div className="login-form">
+                    <div className="form-group">
+                        <label>이메일</label>
+                        <div className="input-wrapper">
+                            <input type="email" placeholder="이메일" />
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <label>비밀번호</label>
+                        <div className="input-wrapper">
+                            <input type="password" placeholder="비밀번호" />
+                        </div>
+                    </div>
+
+                    <div className="button-row">
+                        <Button variant="gray" onClick={handleLogin}>Login</Button> 
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 }
